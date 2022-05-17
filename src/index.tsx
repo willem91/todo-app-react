fc5ff3342bf7f9173d59116691b15d5a69a45094
willem.deloumeaux <deloumwi@gmail.com>
2022-05-17 15:02:13 +0200

    test1

--- src/index.tsx
+++ src/index.tsx
@@ -1,84 +1,22 @@
-//On importe la librairie react
+// On importe la librairie react
 import React from 'react'
-//On importe la libraie react-dom(elle permetd'affcher react sur la page
+// On importe la librairie react-dom (elle permet
+// d'afficher react sur la page)
 import { createRoot } from 'react-dom/client'
-// const name: string = 'John doe'
-// const id: string = 'ma-super-div'
-// const notes: Array<number> = [12, 15, 8, 9]
-// const age: number = 18
-//On crée un element react(jsx)
-// const element: JSX.Element = (
-//   <div className="test" id={`${name}-${id}`}>
-//     <h1>Bonjour'{name}</h1>
-//     <p>Coucou les amis</p>
-//     <p>
-//       {age >= 18 ? (
-//         <div>
-//           <p>Vous etes majeur</p>
-//         </div>
-//       ) : (
-//         <div>
-//           <p>Vous etes mineur</p>
-//         </div>
-//       )}
-//     </p>
-//     <ul>
-//       {notes.map((note, index) => (
-//         <li key={`note-${index}`}>{note}/20</li>
-//       ))}
-//     </ul>
-//   </div>
-// )
-// function Hello(): JSX.Element {
-//   return <p>Hello tout le monde </p>
-// }
+// On importe le composant App (le composant qui contiendra
+// l'intégralité de notre application)
+import App from './App'
 
-// const element: JSX.Element = (
-//   <div>
-//     <Hello />
-//   </div>
-// )
-//? =>age est facutatif
-type HelloProps = {
-  name: string
-  age?: string
-}
-
-function Hello({ name, age = '??' }: HelloProps): JSX.Element {
-  return (
-    <p>
-      Hello {name}, vous avez {age} ans :)
-    </p>
-  )
-}
-type BigTextProps = {
-  children: React.ReactNode
-}
-
-function BigText({ children }: BigTextProps): JSX.Element {
-  return (
-    <div className="gros-text">
-      <h1>{children}</h1>
-    </div>
-  )
-}
-const element: JSX.Element = (
-  <div>
-    <Hello name="Rose" />
-    <Hello name="Jean" age="35" />
-    <Hello name="Jack" age="21" />
-    <Hello name="Jane" age="40" />
-  </div>
-)
-
-//On root de notre page recupère la balise div#root de notre page Html
+// On récupére la balise div#root de notre
+// page HTML
 const root = document.querySelector('#root')
 
-//Si il ny a pas de balise root
+// Si il n'y a pas de balise root
 if (!root) {
-  //On leve une erreur
-  throw new Error('Oups... il manque une balise root dans votre index.html')
+  // On léve un erreur
+  throw new Error('Oups ... il manque la balise root dans votre index.html')
 }
-//J'affiche l'element à l'ecran de la balise
-//div#root
-createRoot(root).render(element)
+
+// J'affiche le composant App à l'écran dans la balise
+// div#root
+createRoot(root).render(<App />)
